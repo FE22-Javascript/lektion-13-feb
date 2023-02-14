be8b1f32f8b5817b1215f7e11eaa2b91b80fb3a8
Maja Gedda <maja.gedda@zocom.se>
2023-02-14 10:23:26 +0100

    add new game

--- script.js
+++ script.js
@@ -20,24 +20,30 @@ games.push(candyCrush);
 const rdr = new Game('Red Dead Redemption', 'Rockstar', 'PC', 'Martina');
 games.push(rdr);
 
-console.log(games);
-
 ADD_GAME_BTN.addEventListener('click', () => {
     console.log('spel');
     // ta info från användaren
+    let title = document.querySelector('#title').value;
+    let creator = document.querySelector('#creator').value;
+    let device = document.querySelector('#console').value;
+    let from = document.querySelector('#from').value;
     // göra till ett nytt objekt med hjälp av klasstypen Game
+    let newGame = new Game(title, creator, device, from);
     // läggain det nya objektet i listan games
+    games.unshift(newGame);
     // rendera uppdaterade listan i UI't
+    renderGamesToUI();
 });
 
 renderGamesToUI();
 // rendera spel från games-listan ut till UI't
 function renderGamesToUI() {
     let gamesContainerEl = document.querySelector('.games-container');
+    gamesContainerEl.innerHTML = '';
     games.forEach(game => {
-        console.log(game);
-        gamesContainerEl.innerHTML += `
-            <article class="games-container__card">
+        let gameCardEl = document.createElement('article');
+        gameCardEl.classList.add('games-container__card');
+        gameCardEl.innerHTML = `
                 <figure></figure>
                 <section class="card__info">
                     <h3 class="card__info-title">${game.title}</h3>
@@ -45,7 +51,14 @@ function renderGamesToUI() {
                     <p class="card__info-console">${game.console}</p>
                     <p class="card__info-from">${game.from}</p>
                 </section>
-            </article>
         `;
+        gameCardEl.addEventListener('click', () => {
+            localStorage.setItem('game', )
+            console.log(game.title);
+            console.log(window);
+            window.location.href = 'game.html';
+        });
+        gamesContainerEl.appendChild(gameCardEl);
     });
+
 };
